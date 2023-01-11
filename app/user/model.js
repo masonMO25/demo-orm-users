eab16a590b58d9a112516c2cbfa8ab9d8bf10438
masonMO25 <masonneale@gmail.com>
2023-01-10 21:11:06 -0600

    created model and routes

--- app/user/model.js
+++ app/user/model.js
@@ -8,15 +8,19 @@ const User = sequelize.define(
       type: DataTypes.INTEGER,
       primaryKey: true,
       autoIncrement: true,
+      allowNull: false,
     },
     username: {
       type: DataTypes.STRING,
+      allowNull: false,
     },
     email: {
       type: DataTypes.STRING,
+      allowNull: false,
     },
     password: {
       type: DataTypes.STRING,
+      allowNull: false,
     },
   },
   {
